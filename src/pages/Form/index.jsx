d681c2607011c411217dd50324d86e4ceb167f12
haoyundong <277079581@qq.com>
2019-11-06 17:04:19 +0800

    test

--- src/pages/Form/index.jsx
+++ src/pages/Form/index.jsx
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import ColumnForm from './components/ColumnForm';
+import styles from './index.module.scss';
 
-export default function() {
-  return (
-    <div className="Form-page">
-      <ColumnForm />
-    </div>
-  );
+export default function () {
+
+    return (
+        <div className="Form-page">
+          <ColumnForm />
+        </div>
+
+        // <div className={styles.container}>
+        //     <button onClick={timer} >点我</button>
+        //     <label>倒计时:{time}</label>
+        // </div>
+    );
 }
